50392787a54f3759e246efeeef66d2e0ab75c2c2
Simenf05 <106535609+Simenf05@users.noreply.github.com>
2022-08-29 10:50:41 +0200

    simen-kode og

--- telefonkatalog.py
+++ telefonkatalog.py
@@ -58,3 +58,56 @@ def visAllePersoner ():
 
             input("Trykk en tast for å gå tilbake til menyen")
             printMeny()
+            
+
+
+
+def sokPerson():
+    if not telefonkatalog:
+        print("Det er ingen registrerte perosner i katalogen")
+        printMeny()
+    else:
+        print("1. Søk på fornavn")
+        print("2. Søk på etternavn")
+        print("3. Søk på telefon")
+        print("4. Søk på Tilbake til hovedmeny")
+        sokefelt = input("Skrive inn ønsket søk 1-3, eller 4 for å gå tilbake:")
+        if sokefelt == "1":
+            navn = input("Fornavn:")
+            finnPerson("fornavn", navn)
+        elif sokefelt == "2":
+            navn = input("Etternavn:")
+            finnPerson("etternavn", navn)
+        elif sokefelt == "3":
+            tlfnummer = input("Telefonnummer:")
+            finnPerson("telefonnummer", tlfnummer)
+        elif sokefelt == "4":
+            printMeny()
+        else:
+            print("Ugyldig valg. Velg et tall mellom 1-4.")
+            sokPerson()
+
+# typeSok angir om man søker på fornavn, etternavn eller telefonnummer
+
+def finnPerson(typeSok, sokeTekst):
+    for personer in telefonkatalog:
+        if typeSok == "fornavn":
+            if personer[0] == sokeTekst:
+                print("{0} {1} har telefonnummer {2}".format(personer[0], personer[1], personer[2]))
+            else:
+                print("Finner ingen registrerte personer med fornavn " + sokeTekst)
+                sokPerson()
+        elif typeSok == "etternavn":
+            if personer[1] == sokeTekst:
+                print("{0} {1} har telefonnummer {2}".format(personer[0], personer[1], personer[2]))
+            else:
+                print("Finner ingen registrerte personer med etternavn " + sokeTekst)
+                sokPerson()
+        elif typeSok == "telefonnummer":
+            if personer[2] == sokeTekst:
+                print("Telefonnummer {0} tilhører {1} {2}".format(personer[2], personer[0], personer[1]))
+            else:
+                print("Telefonnummer " + sokeTekst + " er ikke registrert.")
+                sokPerson()
+
+printMeny()
